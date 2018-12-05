bcee7eeeda3e10e9d3f7add95d2580bfecb46b60
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2018-12-05 01:17:50 +0000

    Updated data generation based on Iain's suggestion

--- code/simulation.py
+++ code/simulation.py
@@ -33,7 +33,7 @@ def generate_random_particle():
     x = round(random.uniform(0, grid_width), 2)
     y = round(random.uniform(0, grid_height), 2)
     update_points_dict(0, x, y, 0, 0)
-    
+
     return x, y
 
 def display_next_point(i, x, y):
@@ -42,33 +42,26 @@ def display_next_point(i, x, y):
     ax.scatter(x, y, color=color_code)
     ax.annotate(i, (x, y))
     plt.pause(0.1)
-    
+
     # print('X_'+ str(i), ': ', x, 'Y_' + str(i), ': ', y)
 
 def generate_points():
     # Parameters for the random particle generation
-    sigma = 1e6
+    sigma = 0.001
     theta = 0
-    g = random.uniform(-1, 1)
-    
+
     x_generated, y_generated = 0, 0 # generate_random_particle()
     # Algorithm is based on https://en.wikipedia.org/wiki/Monte_Carlo_method_for_photon_transport?
     for i in range(number_of_all_steps):
         point = [x_generated, y_generated]
         points.append(point)
-        
+
         # random.uniform(0, 1) - sample from range distribution
         s = random.uniform(0, 1)
-        
+
         x_generated = x_generated + s * math.cos(theta)
         y_generated = y_generated + s * math.sin(theta)
-        
-        if g==0:
-            ctnew = 1-2*random.uniform(0, 1)
-        else:
-            pow_g_2 = math.pow(g, 2)
-            ctnew = (1 + pow_g_2 - math.pow((1 - pow_g_2) / (1 - g + 2*g*random.uniform(0, 1)), 2)) / (2*g)
-        
+
         # np.random.randn() - sample from the standard distribution
         t = (2*np.random.randn()*sigma)*math.pi
         theta = theta + t
@@ -109,7 +102,7 @@ weights = tf.Variable(tf.truncated_normal([num_hidden_units, number_of_coordinat
 
 biases = tf.Variable(tf.truncated_normal([number_of_coordinates]))
 
-rnn_cell = tf.contrib.rnn.BasicRNNCell(num_units=num_hidden_units)
+rnn_cell = tf.nn.rnn_cell.LSTMCell(num_units=num_hidden_units)
 
 outputs, state = tf.nn.dynamic_rnn(rnn_cell, inputs=X, dtype=tf.float32)
 
@@ -126,26 +119,26 @@ total_loss = tf.reduce_mean(loss)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss=total_loss)
 
 with tf.Session() as sess:
-    
+
     sess.run(tf.global_variables_initializer())
-    
+
     X_train, Y_train = generate_input_output_data()
-    
+
     for epoch in range(number_of_epochs):
         iter = 0
-        
+
         for _ in range(number_of_batches):
-            
+
             training_x = X_train[iter:iter+batch_size]
-            
+
             training_y = Y_train[iter:iter+batch_size]
-            
+
             _, current_total_loss, pred = sess.run([optimizer, total_loss, prediction], feed_dict={X: training_x, Y: training_y})
-            
+
             print("Epoch: %s/%s; Iteration: %s/%s; Loss: %s;" % (epoch, number_of_epochs, iter, number_of_data_steps, current_total_loss))
-            
+
             iter += batch_size
-            
+
             print("__________________")
 
 # Generate
